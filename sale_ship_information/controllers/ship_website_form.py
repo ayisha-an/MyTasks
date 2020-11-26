f7d861a94681933c029e3699b332c23d6afdd44d
Ayisha Nida <ayisha1359@gmail.com>
2020-11-26 07:42:51 +0530

    changes in template

--- sale_ship_information/controllers/ship_website_form.py
+++ sale_ship_information/controllers/ship_website_form.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
+"""Ship object creation form in website"""
 from odoo import http
 from odoo.http import request
 
 
 class WebsiteShipInfo(http.Controller):
+    """Add ship information from website"""
 
     @http.route('/shipinfo', type='http', auth='public', website=True, sitemap=False)
-    def ship_webform(self, **kwargs):
+    def ship_webform(self):
+        """render ship object creation form when ship information menu item is clicked"""
         partners = request.env['res.partner'].search([])
         return http.request.render('sale_ship_information.createship_form', {
             'partners': partners
@@ -14,6 +17,6 @@ class WebsiteShipInfo(http.Controller):
 
     @http.route('/create/webship', type='http', auth='public', website=True, sitemap=False)
     def create_ship(self, **kwargs):
-        print('data', kwargs)
+        """Create ship object and render a page with creation success message"""
         request.env['sale.ship_info'].create(kwargs)
         return http.request.render('sale_ship_information.creation_success', {})
